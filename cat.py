f23633528319f867607dde8e83b039f35fbedee4
naveenavusali18 <naveen.avusali@everest.engineering>
2023-03-27 11:32:56 +0530

    cat is created

---
+++ cat.py
@@ -0,0 +1,9 @@
+class animal:
+    def eat(self):
+        print("I can eat")
+    def sleep(self):
+        print("I can sleep")
+    def fly(self):
+        print("i cannot fly")
+    def jump(self):
+        print("I can jump")
